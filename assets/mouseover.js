15f165ed843046b8a51b4c044de8a5fe35284505
p12farm <p12farm@ionio.gr>
2018-11-07 15:05:07 +0200

    Update mouseover.js

--- assets/mouseover.js
+++ assets/mouseover.js
@@ -1,16 +1,13 @@
-
-function playclip() {
-if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
-if (document.all)
- {
-  document.all.sound.src = "Sound_Effects.mp3";
- }
-}
-
-else {
+function playclip() 
 {
-var audio = document.getElementsByTagName("audio")[0];
-audio.play();
-}
-}
+   if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) 
+   {
+       if (document.all)
+          document.all.sound.src = "click.mp3";
+    } 
+    else 
+    {
+        var audio = document.getElementsByTagName("audio")[0];
+        audio.play();
+    }
 }
